8d6b2e806c7823cc570fc64055401cd7b8c2100b
jahida <jahidatamanna8573@gmail.com>
2023-07-17 22:21:22 +0600

    nav add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,10 @@ const HomePage = () => {
         <>
             <div className="bg-slate-700 p-8">
                 this is a box
+               
             </div>
+            <div>this is another box</div> 
+            <nav>th is nav</nav>
         </>
 
     )
